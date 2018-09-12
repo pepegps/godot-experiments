4d75a3539b0f25ccc212556399bcd4605cf31e55
giuseppe.silva <giuseppe@ortizgaming.com>
2018-09-12 15:43:01 -0300

    Add line edit to set target value and trigger button

--- Node2D.gd
+++ Node2D.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
-const ANIMATION_STEP_TIME = 10
+const ANIMATION_STEP_TIME = 30
 
 func _ready():
 	var label = get_node("Label")
-	var label2 = get_node("Label2")
 	var tween = get_node("Tween")
-	var target_value = 100
 	label.set_text(str(0))
-	update_animation_label(label,int(label.get_text()), target_value, tween)
 	var tween2 = Tween.new()
 	var parent = get_tree().get_root().get_child(0)
 	parent.add_child(tween2)
@@ -16,6 +13,9 @@ func _ready():
 	
 func update_animation_label(label, current_value, end_value, tween):
 #	print("update_animation_label %s %s" % [label.name , label.get_class()])
+	if tween.is_processing() :
+		tween.stop()
+
 	tween.interpolate_method(label, "to_text", current_value, end_value, ANIMATION_STEP_TIME,Tween.TRANS_QUAD, Tween.EASE_OUT)
 	tween.start()
 #	print (tween)
